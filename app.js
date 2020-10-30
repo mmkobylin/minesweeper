cd337c0e0b933efe3e2dba05b08da86fbd7866ba
mmkobylin <mmkobylin@gmail.com>
2020-10-30 17:02:19 +0000

    bombs and empies array created and console.log

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     
     let width = 10
+    let bombAmount = 20
     let squares = []
     
     //createBoard 
     function createBoard(){
+
+        //randomise the bombs 
+
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('empty')
+
+        console.log(bombsArray)
+        console.log(emptyArray)
+
         for(let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             //creating separate id for each item
