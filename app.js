dd026f36aa6e9ea69dc624f674e0bc854375d274
mmkobylin <mmkobylin@gmail.com>
2020-11-27 13:57:35 +0000

    amount of bombs and empty spaces added

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const grid = document.querySelector('.grid')
 
-    let width = 10; 
+    let width = 10
 
     //squares being an empty array
     let squares = []; 
 
+    let bombAmount = 20
+
     // create board
     function createBoard() {
+        //adding bomb property to the squares
+        const bombsArray = Array(bombAmount).fill('bomb')
+
+        //adding empties - total - amount of bombs
+        const emptyArray = Array(width*width - bombAmount).fill('empty')
+
+        console.log(bombsArray);
+        console.log(emptyArray);
+
+
         //making 100 items
         for(let i = 0; i < width*width; i++) {
             //creating div elements
@@ -18,11 +30,11 @@ document.addEventListener('DOMContentLoaded', () => {
             square.setAttribute('id', i)
             //appending them to grid
             grid.appendChild(square)
-
+            //square pushed to an empty array
             squares.push(square)
         }
     }
-
+    //function invoked
     createBoard()
 
 })
