f3b0cf0f583374bc0717fdd92b5879b9d0a914cc
mmkobylin <mmkobylin@gmail.com>
2020-11-27 13:47:17 +0000

    app.js resetted, grid squares created

--- app.js
+++ app.js
@@ -1,61 +1,28 @@
-// making sure the content is first loaded
+//loading the entire web
 document.addEventListener('DOMContentLoaded', () => {
 
-    //picking the grid class item and making it a variable
     const grid = document.querySelector('.grid')
-    
-    let width = 10
-    let bombAmount = 20
-    let squares = []
-    
-    //createBoard 
-    function createBoard(){
 
-        //bombs array declared
-        const bombsArray = Array(bombAmount).fill('bomb')
-        
-        //empty array declared
-        const emptyArray = Array(width*width - bombAmount).fill('empty')
+    let width = 10; 
 
-        //creating combined array emptyArray with bombArray
-        const gameArray = emptyArray.concat(bombsArray)
+    //squares being an empty array
+    let squares = []; 
 
-        //shuffling array
-
-        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
-        
+    // create board
+    function createBoard() {
+        //making 100 items
         for(let i = 0; i < width*width; i++) {
-            const square = document.createElement('div')
-            //creating separate id for each item
+            //creating div elements
+            const square = document.createElement('div');
+            //attaching number to each square
             square.setAttribute('id', i)
-            //appending to the grid
+            //appending them to grid
             grid.appendChild(square)
+
             squares.push(square)
-            squares[i].classList.add('grid-small')
-            squares[i].classList.add(shuffledArray[i])
-
-                for (let i = 0; i < squares.length; i++ ) {
-                    //is it max on the left?
-                    const isLeftEdge = i % width === 0 
-                    //is it on the right?
-                    const isRightEdge = i % width === width -1 
-                }
-
-                // if this square is not a bomb
-                if (squares[i].classList.contains('empty')) {
-                    //we start with no bombs
-                    total = 0 
-                    // if it is not 0 and not on left edge
-                    //and the square on the left contains bomb 
-                    if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) {
-                        
-                        //add 1 to total                        
-                        total++
-                    }
-
-                }
-            }
-
         }
-        createBoard()
-    })
+    }
+
+    createBoard()
+
+})
