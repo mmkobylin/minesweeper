d11010ee5ef0353e9cec857ba853ec488fda9a0d
mmkobylin <mmkobylin@gmail.com>
2020-11-27 15:03:54 +0000

    logic looking for bombs around the square added

--- app.js
+++ app.js
@@ -44,15 +44,45 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square)
         }
 
-        for (let i = 0; i < squares.length, i++) {
-
+        for (let i = 0; i < squares.length; i++) {
+            
+            let total = 0 
             //is is on the right edge?
             const isLeftEdge = (i % width === 0)
             
             //if i is divided by 10, it leaves 9 === width (10) - 1
-            const ifRightEdge = (i % width === width - 1 )
+            const isRightEdge = (i % width === width - 1 )
 
-            
+            // if it is not a bomb
+            if (squares[i].classList.contains('empty')) {
+                //is the item on left containing bomb?
+                if (i > 0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total ++
+
+                //is the item on right-up containing bomb?
+                if (i > 9 && !isRightEdge && squares[i+1-width].classList.contains('bomb')) total ++
+
+                //is the item directly up containind bomb
+                if (i > 10 && squares[i-width].classList.contains('bomb')) total ++
+
+                //is the item on left-up containing bomb?
+                if (i > 11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb')) total ++
+                
+                //is the item on right containing bomb?
+                if (i > 0 && !isRightEdge && squares[i+1].classList.contains('bomb')) total ++
+
+                //is the item directly below containind bomb
+                if (i < 89 && squares[i+width].classList.contains('bomb')) total ++
+
+                 //is the item on right-down containing bomb?
+                 if (i < 88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total ++
+
+                 //is the item on left-down containing bomb?
+                 if (i < 90 && !isLeftEdge && squares[i-1+width].classList.contains('bomb')) total ++
+ 
+
+                squares[i].setAttribute('data', total)
+                console.log(squares[i])
+            }
 
         }
     }
