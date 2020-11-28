6c5be4e4cc80511a56a165addf62159f4f77e0c9
mmkobylin <mmkobylin@gmail.com>
2020-11-28 10:55:40 +0000

    fixed issues for corner bottom right bomb prediction pattern

--- app.js
+++ app.js
@@ -83,10 +83,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (i < 100 && !isRightEdge && squares[i+1].classList.contains('bomb')) total ++
 
                 //is the item directly below containind bomb
-                if (i < 89 && squares[i+width].classList.contains('bomb')) total ++
+                //changed from 89 to 90 to include last row
+                if (i < 90 && squares[i+width].classList.contains('bomb')) total ++
 
                  //is the item on right-down containing bomb?
-                 if (i < 88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total ++
+                //changed from 89 to 90 to include last row
+                 if (i < 89 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total ++
 
                  //is the item on left-down containing bomb?
                  if (i < 90 && !isLeftEdge && squares[i-1+width].classList.contains('bomb')) total ++
